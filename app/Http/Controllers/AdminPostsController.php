801a7bf489c293e30526a38e6b719f10b11153b5
林江閤 <tkmk7q8qq@gmail.com>
2020-11-30 19:57:13 +0800

    練習4-3新增文章新增路由,4-4新增AdminPostsController內store新方法

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -17,6 +17,10 @@ class AdminPostsController extends Controller
     {
         return view('admin.posts.create');
     }
+    public function store()
+    {
+       
+    }
 
     public function edit($id)
     {
